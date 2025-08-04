5d675a64828c6aaf540b362947f3653852ea99b3
abdulla-rez <abdulla.s@reizend.ai>
2025-08-04 18:30:42 +0530

    Navbar Completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,14 @@
-import React from "react";
-import CustomButton from "./components/ui/CustomButton";
-import CustomInput from "./components/ui/CustomInput";
+
+import Navbar from "./components/NavBar";
+import './index.css'
 
 const App = () => {
   return (
     <div>
-      <CustomInput
-        label="Name"
-        width="400px"
-        sx={{ backgroundColor: "#4684b6ff", borderRadius: "8px" }}
-      />
-      <CustomInput
-        label="Name"
-        width="250px"
-        sx={{ backgroundColor: "#f5f5f5", borderRadius: "8px" }}
-      />
-
-      <CustomButton label="Testing" variant="outlined" />
+      <Navbar/>
     </div>
+
+
   );
 };
 
