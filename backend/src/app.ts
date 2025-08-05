1c1df73dd7220d9d7eaa0635f5eeacf8f23c4d4c
shebin-ks <shebin.ks@reizend.ai>
2025-08-05 13:03:10 +0530

    implemented patient routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { errorHandler } from "./middlewares/errorHandler";
 import cookieParser from 'cookie-parser'
 import userRouter from './routes/auth.routes'
+import patientRoutes from './routes/patient.routes'
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 
 
 app.use("/api/v1/auth", userRouter)
+app.use("/api/v1/patient", patientRoutes)
 
 
 
---
+++ backend/src/routes/patient.routes.ts
@@ -0,0 +1,22 @@
+
+import express from 'express'
+import { getAllPatientsHandler, getPatientByIdHandler, updatePatientHandler } from '../controllers/patient.controllers'
+import { authenticate } from '../middlewares/auth.middleware'
+import { validateBody } from '../middlewares/body.validator.middleware'
+import { updatePatientSchema } from '../validations/patient.validations'
+
+
+const router = express.Router()
+
+
+router.route("/")
+    .get(getAllPatientsHandler)
+
+router.route("/:patientId")
+    .get(authenticate, getPatientByIdHandler)
+
+router.route('/update')
+    .patch(authenticate, validateBody(updatePatientSchema), updatePatientHandler)
+
+
+export default router
