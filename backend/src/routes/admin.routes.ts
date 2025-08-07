d467486f4408557babe788ef8ba39c1654174fe8
shameem-reizend <mohammed.shameem@reizend.ai>
2025-08-07 11:36:54 +0530

    changes in bill user roles in routes

--- backend/src/routes/admin.routes.ts
+++ backend/src/routes/admin.routes.ts
@@ -4,13 +4,14 @@ import { validateBody } from '../middlewares/body.validator.middleware';
 import { updateUserSchema } from '../validations/user.validations';
 import { doctorSchema } from '../validations/doctor.validations';
 import { authenticate, authorize } from '../middlewares/auth.middleware';
+import { UserRole } from '../entities/user.entity';
 
 const adminRoutes = express.Router();
 adminRoutes.use(authenticate)
 
-adminRoutes.get('/all', authorize('admin'), fetchAllUsers);
-adminRoutes.post('/update/:userId', authorize('admin'), validateBody(updateUserSchema), updateUserDetails);
-adminRoutes.delete('/delete/:userId', authorize('admin'), deleteUser);
-adminRoutes.post('/register-doctor', authorize('admin'), validateBody(doctorSchema), registerDoctor);
+adminRoutes.get('/all', authorize(UserRole.ADMIN), fetchAllUsers);
+adminRoutes.post('/update/:userId', authorize(UserRole.ADMIN), validateBody(updateUserSchema), updateUserDetails);
+adminRoutes.delete('/delete/:userId', authorize(UserRole.ADMIN), deleteUser);
+adminRoutes.post('/register-doctor', authorize(UserRole.ADMIN), validateBody(doctorSchema), registerDoctor);
 
 export default adminRoutes;
