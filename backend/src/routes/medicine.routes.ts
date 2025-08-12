63668862b305d02bacfe78d8b1e287f49b9a638c
shameem-reizend <mohammed.shameem@reizend.ai>
2025-08-12 12:36:51 +0530

    api integration - medicine edit and delete

--- backend/src/routes/medicine.routes.ts
+++ backend/src/routes/medicine.routes.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import { authenticate, authorize } from '../middlewares/auth.middleware';
 import { UserRole } from '../entities/user.entity';
-import { createMedicine, deleteMedicine, fetchAllMedicines } from '../controllers/medicine.controller';
+import { createMedicine, deleteMedicine, fetchAllMedicines, updateMedicineHandler } from '../controllers/medicine.controller';
 
 const medicineRoutes = express.Router()
 medicineRoutes.use(authenticate);
 
 medicineRoutes.post('/create',authorize(UserRole.ADMIN),createMedicine)
 medicineRoutes.get('/fetchAll',authorize(UserRole.ADMIN),fetchAllMedicines)
-medicineRoutes.delete('/delete/:id',authorize(UserRole.ADMIN),deleteMedicine)
-
+medicineRoutes.delete('/delete/:id',authorize(UserRole.ADMIN),deleteMedicine);
+medicineRoutes.post('/update/:medicineId', authorize(UserRole.ADMIN), updateMedicineHandler);
 
 
 export default medicineRoutes
