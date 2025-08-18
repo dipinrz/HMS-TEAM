1daf8dde89e86995079ad56a36cbc783c839cbba
Alfin Johnson <alfin.johnson@reizend.ai>
2025-08-18 17:11:20 +0530

    changed patientAPI

--- frontend/src/services/patientApi.ts
+++ frontend/src/services/patientApi.ts
@@ -9,3 +9,11 @@ export const addAppoinment=async(formData:FormData)=>{
     return commonAPI('POST',`${baseURL}/appointment`,formData)
 }
 
+export const getPatientById = async (id: number) => {
+    return await commonAPI('GET', `${baseURL}/patients/${id}`);
+}
+
+export const updatePatient = async (payload: any) => {
+    return await commonAPI('PATCH', `${baseURL}/patients/update`, payload);
+
+}
