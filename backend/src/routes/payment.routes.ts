b1ee8e2cabbb3ae57e4e2986a78203dcbb28808d
shameem-reizend <mohammed.shameem@reizend.ai>
2025-08-21 10:40:27 +0530

    API - payment history

--- backend/src/routes/payment.routes.ts
+++ backend/src/routes/payment.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createRazorpayOrder, getMonthWiseBilling, verifyRazorpayPayment } from '../controllers/payment.controller';
+import { createRazorpayOrder, getMonthWiseBilling, getPaymentHistorybyPatientHandler, verifyRazorpayPayment } from '../controllers/payment.controller';
 import { authenticate, authorize } from '../middlewares/auth.middleware';
 import { UserRole } from '../entities/user.entity';
 
@@ -9,5 +9,6 @@ paymentRoutes.use(authenticate)
 paymentRoutes.post('/create-order', authorize(UserRole.PATIENT, UserRole.ADMIN), createRazorpayOrder);
 paymentRoutes.post('/verify', authorize(UserRole.PATIENT, UserRole.ADMIN), verifyRazorpayPayment);
 paymentRoutes.get('/monthly', authorize(UserRole.ADMIN), getMonthWiseBilling);
+paymentRoutes.get('/history', authorize(UserRole.PATIENT), getPaymentHistorybyPatientHandler);
 
 export default paymentRoutes;
