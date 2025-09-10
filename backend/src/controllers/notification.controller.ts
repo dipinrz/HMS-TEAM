9d1b564ac7d858f01f42e1eacb38af460644af08
Alfin Johnson <alfin.johnson@reizend.ai>
2025-09-10 12:23:39 +0530

    added update status notification api

--- backend/src/controllers/notification.controller.ts
+++ backend/src/controllers/notification.controller.ts
@@ -1,5 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import { notificationRepo } from "../services/notification.services";
+import {
+  getNotificationById,
+  notificationRepo,
+} from "../services/notification.services";
+import { number } from "joi";
+import { Status } from "../entities/notification.entity";
+import { ApiError } from "../utils/apiError";
 
 export const fetchNotification = async (
   req: any,
@@ -16,6 +22,27 @@ export const fetchNotification = async (
     res
       .status(200)
       .json({ success: true, msg: "Notification fetched successfully", data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateStatus = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { notifID } = req.params;
+    const notification = await getNotificationById(Number(notifID));
+    if (!notification) {
+      throw new ApiError("No notification found", 404);
+    }
+    notification.status = Status.READ; // update the status
+
+    await notificationRepo.save(notification); // persist changes
+
+    return res.json({ success: true, data: notification });
   } catch (error) {
     next(error);
   }
