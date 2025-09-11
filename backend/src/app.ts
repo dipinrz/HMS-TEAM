188dbacdfcfad3981f35b82f28579bd264c36ee2
shankarshambhu <ravi.shankar@gmail.com>
2025-09-11 17:25:19 +0530

    resolved cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,7 +20,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:process.env.FRONTEND_URL
+  }
+));
 app.use(express.json());
 app.use(cookieParser())
 
